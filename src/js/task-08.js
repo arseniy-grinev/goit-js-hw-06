360edd566b3a15a668898917a7ffc3cba046e68f
arseniy-grinev <arseniy.grinev@gmail.com>
2022-07-31 13:56:57 +0300

    end tsk8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+// console.log(form);
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const { elements: { email, password } } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены!");
+    }
+
+    const formData = new FormData(event.currentTarget);
+        console.log(`Email: ${email.value}`);
+        console.log(`Password: ${password.value}`);
+    
+    event.currentTarget.reset();
+    
+}
