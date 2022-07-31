eb7537e71541d14cb1406682bfcb70c661c82361
arseniy-grinev <arseniy.grinev@gmail.com>
2022-07-31 14:23:59 +0300

    end tsk9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColorBtn = document.querySelector('.change-color');
+const currentColor = document.querySelector('.color');
+
+console.log(changeColorBtn, currentColor);
+changeColorBtn.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  currentColor.textContent = `${document.body.style.backgroundColor}`;
+}
